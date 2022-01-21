9e11dc2a054095e449c07732d37ee72b4e94dd81
Corbin Crutchley <crutchcorn@gmail.com>
2022-01-21 06:50:02 -0800

    Added TS typings

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+import {ISize} from "image-size/dist/types/interface";
+import type {Root} from 'hast'
+interface Options {
+  dir: string;
+}
+import type {Plugin} from 'unified'
+
+// Note: defining all nodes here, such as with `Root | Element | ...` seems
+// to trip TS up.
+declare const rehypeImgSize: Plugin<[Options] | [], Root, string>
+export default rehypeImgSize
+export type {Options}
+
+export function getImageSize(src: string, dir?: string): ISize;
